c1fd984a0f21fd9d55388a1697124fdede58d37d
Nick Poole <Nick.Poole@SparkFun.com>
2018-03-30 14:38:17 -0600

    Attempting to fix nav double-click bug

--- logic.js
+++ logic.js
@@ -281,17 +281,21 @@ $(".cross").click();}
 
 // Handler for "Add Module" navigation action
 $(".add-module").click(function() {
+  $("exit-foot").css('pointer-events','none');	
   $(".module-list").slideToggle(slideDur, function() {
     $("footer").hide();
     $("exit-foot").show();
+    $("exit-foot").css('pointer-events','auto');		  
   });
 });
 
 // Module list exit handler
 $(".mod-list-exit").click(function() {
+  $("exit-foot").css('pointer-events','none');
   $(".module-list").slideToggle(slideDur, function() {
     $("footer").show();
     $("exit-foot").hide();
+    $("exit-foot").css('pointer-events','auto');
   });
 });
 
@@ -320,9 +324,11 @@ $(targetmodule).find(".act-menu").slideToggle(slideDur, function() {
 }));
 
 function hideActMenu(){
+$("blank-foot").css('pointer-events','none');
 $(".act-menu:visible").slideToggle(slideDur, function() {
     $("actions-foot").show();
-    $("blank-foot").hide();	
+    $("blank-foot").hide();
+    $("blank-foot").css('pointer-events','auto');
   });
 };
 
@@ -419,10 +425,12 @@ function progSlideshow(){
 
 // Hardware Configure exit handler
 $(".program-pane-exit").click(function() {
+  $("program-foot").css('pointer-events','none');
   $(".program-hardware").slideToggle(slideDur, function() {
 	$("#howtoprogram").find("img").remove();
     $("footer").show();
     $("program-foot").hide();
+    $("program-foot").css('pointer-events','auto'); 
   });
 });
 
@@ -439,9 +447,11 @@ $(".share-pane-exit").click(function() {
   $(".share-link").html("");
   if(gapi.auth2.getAuthInstance().isSignedIn.get()){
   $(".save-button-authed").show();}
+  $("share-foot").css('pointer-events','none');	
   $(".share-project").slideToggle(slideDur, function() {
     $("footer").show();
     $("share-foot").hide();
+    $("share-foot").css('pointer-events','auto');	  
   });
 });
 
